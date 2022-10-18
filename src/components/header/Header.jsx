ab4c9652998135204a130d3eb8a6965a5fcd8d6e
Vlad Deputat <deputatwladek@gmail.com>
2022-10-18 19:28:16 +0300

    create basic structure

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="../../assets/Logo.png" alt="logo" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Our mission</li>
+          <li>Places</li>
+          <li>Team</li>
+        </ul>
+      </nav>
+      <a href="#login"></a>
+    </header>
+  );
+};
+
+export default Header;
